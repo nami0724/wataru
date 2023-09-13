a11dc51dfd48671659a3bcd54cea608693bd8b56
Nami Kuroiwa <kuroiwa.nami.18@gmail.com>
2023-09-13 18:27:09 +0900

    １回目

---
+++ resources/views/top/index.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <h1>店の名前</h1>
+    ・場所
+    ・インスタ等
+
+    <nav>
+        <li>
+            <ol>1</ol>
+        </li>
+    </nav>
+</header>
+
+<body>
+    <h2 id=aboutShop>お店について</h2>
+    <h2 id=menu>メニュー</h2>
+    <h2 id=gallery>ギャラリ－</h2>
+    <h2 id=price>料金</h2>
+    <h2 id=question>よくあるご質問</h2>
+    <h2 id=voice>お客様の声</h2>
+    <h2 id=mail>お問い合わせ</h2>
+</body>

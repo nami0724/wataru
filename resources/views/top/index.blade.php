648fd3cd5709d5bcbae95b3bf81855bb70fa3ab5
Nami Kuroiwa <kuroiwa.nami.18@gmail.com>
2023-09-14 10:38:27 +0900

    大枠の追加

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -1,21 +1,65 @@
-<header>
-    <h1>店の名前</h1>
-    ・場所
-    ・インスタ等
+<html lang ="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="△△△△">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="csrf_token">
+    <title>〇〇〇</title>
+    <link rel="stylesheet" href="http://unpkg.com/ress/dist/ress.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header>
+        <h1>店の名前</h1>
+        ・場所
+        ・インスタ等
 
-    <nav>
-        <li>
-            <ol>1</ol>
-        </li>
-    </nav>
-</header>
+        <nav>
+            <ol>
+                <ul><a href="#aboutShop">お店について</a></ul>
+                <ul><a href="#gallery">ギャラリー</a></ul>
+                <ul><a href="#menu">メニュー</a></ul>
+                <ul><a href="#price">料金</a></ul>
+                <ul><a href="#voice">お客様の声</a></ul>
+                <ul><a href="#question">よくあるご質問</a></ul>
+                <ul><a href="#mail">お問い合わせ</a></ul>
+            </ol>
+        </nav>
+    </header>
 
-<body>
-    <h2 id=aboutShop>お店について</h2>
-    <h2 id=menu>メニュー</h2>
-    <h2 id=gallery>ギャラリ－</h2>
-    <h2 id=price>料金</h2>
-    <h2 id=question>よくあるご質問</h2>
-    <h2 id=voice>お客様の声</h2>
-    <h2 id=mail>お問い合わせ</h2>
-</body>
+    
+        <h2 id=aboutShop>お店について</h2>
+        <h2 id=gallery>ギャラリ－</h2>
+        <h2 id=menu>メニュー</h2>
+        <h2 id=price>料金</h2>
+        <h2 id=voice>お客様の声</h2>
+        <h2 id=question>よくあるご質問</h2>
+          <h3>当日について</h3>
+            <h4>Q.持ち物を教えてください</h4>
+            <h4>Q.当日から入浴は可能ですか？</h4>
+            <h4>Q.施術時間はどのくらいですか？</h4>
+            <h4>Q.お店の場所はどこにありますか？</h4>
+          <h3>施術について</h3>
+            <h4>Q.施術時、どのくらい痛むか教えてください</h4>
+            <h4>Q.日焼けしていても施術できますか？</h4>
+            <h4>Q.同時に２名以上の施術は可能ですか？</h4>
+            <h4>Q.肌が弱いのですが、タトゥーを入れても大丈夫でしょうか</h4>
+            <h4>Q.薬を服用していてもタトゥーを入れられますか？</h4>
+            <h4>Q.タトゥーを入れたところが毛深くなることはありますか？</h4>
+          <h3>お支払いについて</h3>
+            <h4>Q.金額の目安を教えてください</h4>
+            <h4>Q.支払い方法を教えてください</h4>
+            <h4>Q.分割払いはできますか</h4>
+            <h4>Q.学割はできますか</h4>
+            <h4>Q.友達紹介などのキャンペーンはありますか</h4>
+          <h3>ご予約について</h3>
+            <h4>Q.予約の取り方を教えてください</h4>
+            <h4>Q.キャンセルの連絡はいつまでにすれば良いですか？</h4>
+            <h4>Q.未成年でも予約可能でしょうか？</h4>
+        <h2 id=mail>お問い合わせ</h2>
+    </body>
+
+    <footer>
+        
+    </footer>
+</html>
